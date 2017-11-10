8641bb43276c64baf1513603bcb728c4e2b115bf
Peter Henell <mail@peterhenell.se>
2017-11-10 14:45:33 +0100

    using selector to select gui to display

---
+++ src/Comparer.GUI/Selectors/ComparerTypeDataTemplateSelector.cs
@@ -0,0 +1,42 @@
+﻿using DataSetTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Comparer.GUI.Selectors
+{
+    public class ComparerTypeDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CsvFileTemplate { get; set; }
+        public DataTemplate QueryTemplate { get; set; }
+        public DataTemplate NotImplementedTemplate { get; set; }
+
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is InputTypes)
+            {
+                var value = (InputTypes)item;
+                switch (value)
+                {
+                    case InputTypes.Query:
+                        return QueryTemplate;
+                    case InputTypes.CsvFile:
+                        return CsvFileTemplate;
+                    case InputTypes.DataTable:
+                        return NotImplementedTemplate;
+                }
+
+                return base.SelectTemplate(item, container);
+            }
+            else
+            {
+                return base.SelectTemplate(QueryTemplate, container);
+            }
+        }
+    }
+}
